9b5b0e72d1012d597ebb658bfbe965bf1eada1bf
SunRedSAMA <923557933@qq.com>
2024-04-27 11:33:01 +0800

    7.2

--- 1.py
+++ 1.py
@@ -1,10 +1,13 @@
-#请补充下面的句子，创建一个名为dic_student的空字典
-dic_student={}
-for i in range(1,6):
-    name=input()
-    age=eval(input())
-    #请在下面的空白处写句子，将用户输入的name当成dic_student的键，age当成值，
-    dic_student[name]=age
-for k,v in dic_student.items():
-    #请补充下面的句子，按要求输出字典中的数据
-    print("{:<6}%d岁".format(k)%v)
+myDict={'方糖':99,'x1':499,'魔盒':399,'曲奇':299}
+#请补充下面的句子，用items方法遍历字典的键和值
+for k,v in myDict.items():
+    print(k+'\t…………\t'+str(v))
+lst=[]
+#请补充下面的句子，用values方法遍历字典的值
+for v in myDict.values():
+    lst.append(v)
+print('所有产品的平均价为：{}'.format(sum(lst)/len(lst)))
+newlst=[(v,k) for k,v in myDict.items()]
+#请在下面空白处写句子，用sort方法对newlst进行升序排列
+newlst.sort()
+print('价格最高的产品是：{}'.format(newlst[-1][1]))

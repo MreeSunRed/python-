d40229d9f5c96e56aa2a7023b55df4c314731433
SunRedSAMA <923557933@qq.com>
2024-04-27 11:29:12 +0800

    7.1

--- 1.py
+++ 1.py
@@ -1,16 +1,10 @@
-# coding=utf-8
-
-# 创建并初始化`source_list`列表
-source_list = []
-while True:
-    try:
-        list_element = input()
-        source_list.append(list_element)
-    except:
-        break
-
-# 请在此添加代码，对source_list列表进行排序等操作并打印输出排序后的列表
-########## Begin ##########
-source_list.sort(reverse=True)
-print(source_list)
-########## End ##########
+#请补充下面的句子，创建一个名为dic_student的空字典
+dic_student={}
+for i in range(1,6):
+    name=input()
+    age=eval(input())
+    #请在下面的空白处写句子，将用户输入的name当成dic_student的键，age当成值，
+    dic_student[name]=age
+for k,v in dic_student.items():
+    #请补充下面的句子，按要求输出字典中的数据
+    print("{:<6}%d岁".format(k)%v)

76e6b5029d8aa5bbc9b210ae68ee183462b5f682
SunRedSAMA <923557933@qq.com>
2024-04-27 11:33:25 +0800

    7.3

--- 1.py
+++ 1.py
@@ -1,13 +1,14 @@
-myDict={'方糖':99,'x1':499,'魔盒':399,'曲奇':299}
-#请补充下面的句子，用items方法遍历字典的键和值
-for k,v in myDict.items():
-    print(k+'\t…………\t'+str(v))
-lst=[]
-#请补充下面的句子，用values方法遍历字典的值
-for v in myDict.values():
-    lst.append(v)
-print('所有产品的平均价为：{}'.format(sum(lst)/len(lst)))
-newlst=[(v,k) for k,v in myDict.items()]
-#请在下面空白处写句子，用sort方法对newlst进行升序排列
-newlst.sort()
-print('价格最高的产品是：{}'.format(newlst[-1][1]))
+n=int(input("请设置加密位移数："))
+dic_convertor={}
+for i in range(26):
+    ming=ord("a")+i   #ming为键的ASCII编码
+    ch=ming+n
+    if ch<=122:
+        mi=ch             #根据题目要求在注释之前补充句子
+    else:
+        mi=ch-122+96             #根据题目要求在注释之前补充句子
+    dic_convertor[chr(ming)]=chr(mi)
+
+mingwen=input("请输入明文：")
+for i in mingwen:
+    print(dic_convertor[i],end="")

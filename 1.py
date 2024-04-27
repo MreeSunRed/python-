c185542d6e28be4191f869e3f84c0c43e491bc53
SunRedSAMA <923557933@qq.com>
2024-04-27 10:48:05 +0800

    6.3

--- 1.py
+++ 1.py
@@ -1,14 +1,8 @@
-import math
-lst_sides=[3,4,5,6,6,6,4,4,3]
-lst_area=[]
+lst_score=[9, 10, 8, 9, 10, 7, 6, 8, 7, 8]
+#请按照题目要求在空白处编写代码
+lst_score.sort()
+lst_score.pop(0)
+lst_score.pop(-1)
 
-for i in range(0,len(lst_sides),3):
-    #请补充下面的语句，使其能通过列表切片的方式获得三角形的边长a，b，c
-    a,b,c= lst_sides[i:i+3]
-    p=(a+b+c)/2
-    #下面的语句是用海伦公式计算三角形的面积，请补充完整
-    s=math.sqrt(p*(p-a)*(p-b)*(p-c))
-    #请在下面的空白处写语句，利用append函数将计算的s添加进lst_area中
-    lst_area.append(s)
-for i in lst_area:
-    print("{:.2f}".format(i))
+
+print("该选手的最终得分为{}".format(sum(lst_score)/len(lst_score)))

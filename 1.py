d02edc2924ea06607ca862e74e3eec89dbd89017
SunRedSAMA <923557933@qq.com>
2024-04-27 10:45:16 +0800

    6.2

--- 1.py
+++ 1.py
@@ -1,10 +1,14 @@
+import math
+lst_sides=[3,4,5,6,6,6,4,4,3]
+lst_area=[]
 
-print(ord("倪"))
-print(chr(20522))
-lst=['甲','乙','丙','丁']
-#请补充下面的循环语句，使其能够遍历列表lst
-for x in lst:
-    #请根据提示内容补充下面的if语句
-    if x=='丙':
-        print(x)
-        break
+for i in range(0,len(lst_sides),3):
+    #请补充下面的语句，使其能通过列表切片的方式获得三角形的边长a，b，c
+    a,b,c= lst_sides[i:i+3]
+    p=(a+b+c)/2
+    #下面的语句是用海伦公式计算三角形的面积，请补充完整
+    s=math.sqrt(p*(p-a)*(p-b)*(p-c))
+    #请在下面的空白处写语句，利用append函数将计算的s添加进lst_area中
+    lst_area.append(s)
+for i in lst_area:
+    print("{:.2f}".format(i))

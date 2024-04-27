cc7ff44f6e8de0a15bcee41478b0490d65fef67b
SunRedSAMA <923557933@qq.com>
2024-04-27 10:21:15 +0800

    6.1

--- 1.py
+++ 1.py
@@ -1,12 +1,10 @@
 
 print(ord("倪"))
 print(chr(20522))
-n=1
-while 1:
-    #请补充下面的if语句
-    if n%2==1 and n%3==0 and n%4==1 and n%5==4 and n%6==3 and n%7==0 and n%8==1 and n%9==0:
-        print("这个盒子里一共有{}个糖果".format(n))
+lst=['甲','乙','丙','丁']
+#请补充下面的循环语句，使其能够遍历列表lst
+for x in lst:
+    #请根据提示内容补充下面的if语句
+    if x=='丙':
+        print(x)
         break
-    else:
-        n=n+1
-
